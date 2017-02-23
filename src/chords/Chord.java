f77e688e003eac4dc71b16b3c38035ddfe58bb42
Ivan Kuznetsov <kuznecov.fd0@yandex.ru>
2017-02-23 22:57:39 +0300

    Добавил класс с аккордами

---
+++ src/chords/Chord.java
@@ -0,0 +1,39 @@
+package chords;
+
+/**
+ * Created by paul-simon on 23.02.17.
+ */
+public class Chord {
+    final int delta = 5;
+    private String name;
+    private double[] freqArr;
+
+    public Chord(String name, double[] freqArr) {
+        this.name = name;
+        this.freqArr = freqArr;
+    }
+
+    public double match(double[] freqArr, double[] ampl) {
+        double minDelta = 0;
+        double curAmpl = 0;
+        double curFreq = 0;
+        double matchVal = 0;
+
+        for (int i = 0; i < freqArr.length; i ++) {
+            minDelta = 99999999;
+            for (int j = 0; j < this.freqArr.length; j ++) {
+                if (Math.abs(freqArr[i] - this.freqArr[j]) < minDelta) {
+                    minDelta = Math.abs(freqArr[i] - this.freqArr[j]);
+                    curAmpl = ampl[i];
+                    curFreq = freqArr[i];
+                }
+
+                if (minDelta < delta) {
+                    matchVal += curAmpl / minDelta;
+                }
+            }
+        }
+
+        return matchVal;
+    }
+}
---
+++ src/chords/ChordMatcher.java
@@ -0,0 +1,24 @@
+package chords;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by paul-simon on 23.02.17.
+ */
+public class ChordMatcher {
+    List<Chord> chords;
+
+    public ChordMatcher()
+    {
+        this.chords = new ArrayList<Chord>();
+        chords.add(new Chord("Am", new double[] {0}));
+        chords.add(new Chord("C", new double[] {0}));
+        chords.add(new Chord("Dm", new double[] {0}));
+        chords.add(new Chord("G", new double[] {0}));
+        chords.add(new Chord("Em", new double[] {0}));
+        chords.add(new Chord("H7", new double[] {0}));
+    }
+
+    //public match()
+}
