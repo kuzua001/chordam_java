cbb0ef525112ea3e97618a1003b86885473c5b2f
Ivan Kuznetsov <kuznecov.fd0@yandex.ru>
2017-02-24 00:37:39 +0300

    Попробовал добавить звук

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import chords.Core;
 import gen.StringSpectreGenerator;
 import math.Convolve;
 import math.Fft;
@@ -20,79 +21,8 @@ public class Main {
         System.setOut(ps);
     }
 
-    public static void fouirier() throws FileNotFoundException {
-
-
-        double[] list = AmplitudeLoader.loadFromFile("input.txt");
-        int chunkSize = 4096;
-        int sampleRate = 44100;
-        int maxFreq = 2000;
-
-        double[] matchingFreq = new double[] {
-                82.0, 110.0, 146.0, 196.0, 247.0, 329.0,
-                146.0 * Math.pow(2, 1.0/6.0),
-                196.0 * Math.pow(2, 1.0/6.0),
-                247.0 * Math.pow(2, 1.0/12.0),
-        };
-        double[] freqSpectresNormalize = new double[matchingFreq.length];
-/*
-        double[][] freqSpectres = new double[matchingFreq.length][chunkSize];
-        for (int i = 0; i < matchingFreq.length; i ++) {
-            freqSpectres[i] = StringSpectreGenerator.getSpectre(matchingFreq[i], chunkSize, sampleRate, 3);
-            freqSpectresNormalize[i] = 1.0 / Convolve.sum(freqSpectres[i]);
-        }
-*/
-
-        double t = 0;
-        double w = 0;
-        double[] real    = new double[chunkSize];
-        double[] imag    = new double[chunkSize];
-        double[] spectre = new double[chunkSize];
-        double[] convRes = new double[matchingFreq.length];
-        double amp = 0;
-
-        redirectOutput("output.txt");
-
-        for (int i = 0; i < list.length - chunkSize; i += chunkSize) {
-            t = i / (double) sampleRate;
-
-            for (int j = 0; j < chunkSize; j ++) {
-                real[j] = list[i + j];
-                imag[j] = 0;
-            }
-
-            Fft.transform(real, imag);
-            for (int j = 0; j < chunkSize; j ++) {
-                w = j / (double) chunkSize * sampleRate;
-                amp = imag[j] * imag[j] + real[j] * real[j];
-                //amp = Math.pow(amp, 2);
-                System.out.format("%f\t%f\t%f\n", t, amp, w);
-            }
-
-          /*  for (int k = 0; k < matchingFreq.length; k ++)
-            {
-                convRes[k] = Convolve.convolve(spectre, freqSpectres[k], 0, chunkSize * maxFreq / sampleRate) *
-                        freqSpectresNormalize[k];
-            }*/
-            //int[] topFreqIndexes = ArrayAlghoritms.indexesOfTopElements(convRes, 3);
-
-            /*for (int k = 0; k < topFreqIndexes.length; k ++) {
-                System.out.print(matchingFreq[topFreqIndexes[k]] + "\t");
-            }*/
-            System.out.println();
-        }
-    }
-
-    public static void spectre() throws FileNotFoundException {
-        redirectOutput("output.txt");
-        double[] spectre = StringSpectreGenerator.getSpectre(329.0, 4096, 44100, 1);
-        for (int i = 0; i < spectre.length; i ++) {
-            System.out.format("%d\t%f\n", i, spectre[i]);
-        }
-    }
-
     public static void main(String[] args) throws FileNotFoundException {
-        //spectre();
-        fouirier();
+        Core c = new Core();
+        c.run();
     }
 }
--- src/chords/Core.java
+++ src/chords/Core.java
@@ -1,10 +1,50 @@
 package chords;
 
+import javax.sound.sampled.*;
+import java.util.Vector;
+
 /**
  * Created by paul-simon on 23.02.17.
  */
 public class Core {
-    public void run() {
 
+
+
+    public void run() {
+        AudioFormat format = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100, 16, 1, 2, 44100, false);
+        TargetDataLine line;
+        DataLine.Info info = new DataLine.Info(TargetDataLine.class,
+                format); // format is an AudioFormat object
+
+
+        if (!AudioSystem.isLineSupported(info)) {
+            // Handle the error ...
+
+        }
+// Obtain and open the line.
+        try {
+            line = (TargetDataLine) AudioSystem.getLine(info);
+            line.open(format);
+
+
+            line.start();
+
+            for (int t = 0; t < 1000; t ++) {
+                int chunkSize = 4096;
+                byte[] buff = new byte[chunkSize * 2];
+                line.read(buff, 0, chunkSize);
+                double[] pcm = new double[chunkSize];
+                for (int i = 0; i < chunkSize ; i ++) {
+                    pcm[i] = 256 * buff[i * 2] + buff[i * 2 + 1];
+                    System.out.println(pcm[i]);
+                }
+            }
+
+
+
+
+        } catch (LineUnavailableException ex) {
+            // Handle the error ...
+        }
     }
 }
